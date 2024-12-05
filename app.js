2f0e1144479f9b355482e4f8a7d6a66ea53d91aa
SravyaGeethikaSala <salasravyageethika@gmail.com>
2024-12-04 18:07:17 -0600

    Commit

--- app.js
+++ app.js
@@ -86,30 +86,154 @@
 
 // module.exports = app;
 // app.js
+
+// const createError = require('http-errors');
+// const express = require('express');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
+
+// passport.use(new LocalStrategy(function(username, password, done) {
+//   Account.findOne({ username: username }).then(function (user){
+//     if (err) {
+//       return done(err);
+//     }
+//     if (!user) {
+//       return done(null, false, { message: 'Incorrect username.' });
+//     }
+//     if (!user.validPassword(password)) {
+//       return done(null, false, { message: 'Incorrect password.' });
+//     }
+//     return done(null, user);
+//   }).catch(function(err){
+//     return done(err)
+//   })
+// }))
+// require('dotenv').config();
+
+// const connectionString = process.env.MONGO_CON;
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect(connectionString);
+
+// var db = mongoose.connection;
+
+// //Bind connection to error event
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once("open", function(){
+//   console.log("Connection to DB succeeded")
+// });
+
+// // Route imports
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
+// const gridRouter = require('./routes/grid');
+// const pickRouter = require('./routes/pick');
+// const resourceRouter = require('./routes/resource');
+// const instrumentRouter = require('./routes/instrument');
+
+// // Schema definition
+// const instrumentSchema = new mongoose.Schema({
+//   instrument_name: String,
+//   instrument_material: String,
+//   instrument_price: Number
+// });
+
+// const Instrument = mongoose.models.Instrument || mongoose.model('Instrument', instrumentSchema);
+
+// // Database seeding function
+// async function recreateDB() {
+//   await Instrument.deleteMany();
+//   let instance1 = new Instrument({ instrument_name: 'Guitar', instrument_material: 'Wood', instrument_price: 15000  });
+//   let instance2 = new Instrument({ instrument_name: 'Piano', instrument_material: 'Felt', instrument_price: 20000 });
+//   let instance3 = new Instrument({ instrument_name: 'Violin', instrument_material: 'Steel', instrument_price: 10000 });
+
+//   await instance1.save().then(doc => {
+//     console.log("First object saved:", doc);
+//   }).catch(err => {
+//     console.error(err);
+//   });
+
+//   await instance2.save().then(doc => {
+//     console.log("Second object saved:", doc);
+//   }).catch(err => {
+//     console.error(err);
+//   });
+
+//   await instance3.save().then(doc => {
+//     console.log("Third object saved:", doc);
+//   }).catch(err => {
+//     console.error(err);
+//   });
+// }
+
+// let reseed = true;
+// if (reseed) {
+//   recreateDB();
+// }
+
+// // Express app setup
+// const app = express();
+
+// // View engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+// // Middleware
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Routes
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/instrument', instrumentRouter);
+// app.use('/grid', gridRouter);
+// app.use('/selector', pickRouter);
+// app.use('/resource', resourceRouter);
+
+// // passport config
+// // Use the existing connection
+// // The Account model 
+// var Account =require('./models/account');
+// passport.use(new LocalStrategy(Account.authenticate()));
+// passport.serializeUser(Account.serializeUser());
+// passport.deserializeUser(Account.deserializeUser());
+
+// app.use(require('express-session')({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: false
+//  }));
+//  app.use(passport.initialize());
+//  app.use(passport.session());
+ 
+
+// // Error handling
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// app.use(function(err, req, res, next) {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app; 
+// app.js
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(function(username, password, done) {
-  Account.findOne({ username: username }).then(function (user){
-    if (err) {
-      return done(err);
-    }
-    if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
-    }
-    if (!user.validPassword(password)) {
-      return done(null, false, { message: 'Incorrect password.' });
-    }
-    return done(null, user);
-  }).catch(function(err){
-    return done(err)
-  })
-}))
+const passport = require('passport');
 require('dotenv').config();
 
 const connectionString = process.env.MONGO_CON;
@@ -122,7 +246,7 @@ var db = mongoose.connection;
 
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
+db.once("open", function () {
   console.log("Connection to DB succeeded")
 });
 
@@ -133,6 +257,27 @@ const gridRouter = require('./routes/grid');
 const pickRouter = require('./routes/pick');
 const resourceRouter = require('./routes/resource');
 const instrumentRouter = require('./routes/instrument');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
 
 // Schema definition
 const instrumentSchema = new mongoose.Schema({
@@ -146,7 +291,8 @@ const Instrument = mongoose.models.Instrument || mongoose.model('Instrument', in
 // Database seeding function
 async function recreateDB() {
   await Instrument.deleteMany();
-  let instance1 = new Instrument({ instrument_name: 'Guitar', instrument_material: 'Wood', instrument_price: 15000  });
+
+  let instance1 = new Instrument({ instrument_name: 'Guitar', instrument_material: 'Wood', instrument_price: 15000 });
   let instance2 = new Instrument({ instrument_name: 'Piano', instrument_material: 'Felt', instrument_price: 20000 });
   let instance3 = new Instrument({ instrument_name: 'Violin', instrument_material: 'Steel', instrument_price: 10000 });
 
@@ -186,6 +332,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
@@ -196,29 +351,17 @@ app.use('/grid', gridRouter);
 app.use('/selector', pickRouter);
 app.use('/resource', resourceRouter);
 
-// passport config
-// Use the existing connection
-// The Account model 
-var Account =require('./models/account'));
+var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
- }));
- app.use(passport.initialize());
- app.use(passport.session());
- 
-
 // Error handling
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
--- models/instrument.js
+++ models/instrument.js
@@ -9,12 +9,39 @@
 // const Instrument = mongoose.model('Instrument', instrumentSchema);
 
 // module.exports = Instrument;
+
+
+
+// const mongoose = require("mongoose");
+
+// const instrumentSchema = mongoose.Schema({
+//   instrument_name: String,
+//   instrument_material: String,
+//   instrument_price: Number
+// });
+
+// module.exports = mongoose.model("Instrument", instrumentSchema); 
+
 const mongoose = require("mongoose");
 
 const instrumentSchema = mongoose.Schema({
-  instrument_name: String,
-  instrument_material: String,
-  instrument_price: Number
+  instrument_name: {
+    type: String,
+    required: true,
+    minlength: [3, 'Name must be at least 3 characters long'],
+    maxlength: [50, 'Name cannot exceed 50 characters']
+  },
+  instrument_material: {
+    type: String,
+    required: true,
+    
+  },
+  instrument_price: {
+    type: Number,
+    required: true,
+    min: [1000, 'Price should atleast be 1000'],
+    max: [25000, 'Price must not exceed 25000']
+  }
 });
 
-module.exports = mongoose.model("Instrument", instrumentSchema);
+module.exports = mongoose.model("instrument", instrumentSchema);
